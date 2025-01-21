cbad2571c7d1dd485b70d09c18e45ad40c9f4123
John William Bowen <johnwb1776@gmail.com>
2025-01-21 15:13:50 -0500

    updated package

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import type {LoadContext, Plugin} from '@docusaurus/types';
+import type {PluginOptions} from './options';
+
+
+
+
+
+export default function pluginHubspotTackingCode(
+  _context: LoadContext,
+  options: PluginOptions,
+): Plugin | null {
+  
+  if (process.env.NODE_ENV !== 'production') {
+    return null;
+  }
+
+  return {
+    name: 'docusaurus-plugin-hubspot',
+
+    contentLoaded({actions}) {
+      actions.setGlobalData(options);
+    },
+
+    injectHtmlTags() {
+      return {
+        headTags: [
+          {
+            tagName: 'script',
+            attributes: {
+              async: options.async,
+              defer: options.defer,
+              type: 'text/javascript',
+              id: 'hs-script-loader',
+              src: `//js.hs-scripts.com/${options.accountId}.js`,
+            },
+          }
+        ]
+      }
+    }
+  }
+}
+export type {PluginOptions, Options} from './options';
+
+export {validateThemeConfig, validateOptions} from './options';
